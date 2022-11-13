09aa089357fec078aa24b808d57883e7f3c7d493
Shiba-Patrick <liu21407@gmail.com>
2022-11-14 02:08:48 +0800

    database set

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 //宣告及取得需要的變數 express/handlebars
 const express = require('express')
 const express_hbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
-//setting handlebars engine
+//setting handlebars engine & static files & body-parse
 app.engine('handlebars', express_hbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-//setting static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+//setting env 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+//Mongoose DataBase setting
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+//connected error:on
+db.on('error', () => {
+  console.log('mongoose error!!!!')
+})
+
+//connected success:once
+db.once('open', () => {
+  console.log('mongo-db connected!!!')
+})
 
 //router setting:模板關鍵字為{#each:name : 封包資料的物件名稱}
 app.get('/', (req, res) => {

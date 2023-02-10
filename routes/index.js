a0d0250c5791889760c0cfe3b7b7bf913c8c910e
Shiba-Patrick <liu21407@gmail.com>
2023-02-10 10:35:51 +0800

    add user model

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router() //load express.Router package
 const home = require('./modules/home')
 const restaurantList = require('./modules/restaurant')
+const users = require('./modules/users')
 
 //load homepage and every function
 router.use('/', home)
 router.use('/restaurants', restaurantList)
+router.use('/users', users)
 
 //exports this router
 module.exports = router

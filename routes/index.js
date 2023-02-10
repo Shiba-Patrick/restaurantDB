a40634417b71b44ec57a6b653fbbc08e1df9cfde
Shiba-Patrick <liu21407@gmail.com>
2023-02-10 12:26:25 +0800

    add user authentication in restaurant and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const home = require('./modules/home')
 const restaurantList = require('./modules/restaurant')
 const users = require('./modules/users')
 
+const { authenticator } = require('../middleware/auth') // 掛載 middleware
+
 //load homepage and every function
-router.use('/', home)
-router.use('/restaurants', restaurantList)
+
+router.use('/restaurants', authenticator, restaurantList)  // 加入驗證程序
 router.use('/users', users)
+router.use('/', authenticator, home)  // 加入驗證程序
 
 //exports this router
 module.exports = router

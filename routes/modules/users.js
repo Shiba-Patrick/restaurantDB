d10441a2848272f8fb39d83a321367c0e9f62d49
Shiba-Patrick <liu21407@gmail.com>
2023-02-10 10:44:35 +0800

    add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,17 @@
 const express = require("express")
+const { route } = require("./home")
 const router = express.Router()
 
 router.get('/login', (req, res)=>{
   res.render('login')
+})
+
+router.post('/login', (req, res)=>{
+
+})
+
+router.get('/register', (req, res)=>{
+  res.render('register')
 })
 
 module.exports = router

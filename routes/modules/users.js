2bc16052cbd1ba287325b738611a06eaa7817cc3
Shiba-Patrick <liu21407@gmail.com>
2023-02-11 09:57:48 +0800

    feat: add bcrypt to login route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const passport = require('passport')
-const bcrypt =require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 const User = require('../../models/user')
 
